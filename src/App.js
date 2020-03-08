7bded3814a2b48214fb902452f905f3f140a97ac
Vimaladevi Moorthy <vimala.may25@gmail.com>
2020-03-08 04:12:03 -0500

    gkfgjldf

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App1">
+      <header className="App-header1">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/fullstack.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
